9113ded5613cfb8d3e693d8ddccf601935f9fc34
Moisès Gómez Girón <moiatgit@gmail.com>
2018-07-05 19:26:05 +0200

    packaged

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
         description = "API to allow execution of SQL statements on csv files",
         long_description = long_description,
         long_description_content_type = "text/markdown",
-        url = "https://github.com/moiatgit/csvsql.git",
+        url = "https://github.com/moiatgit/csvsql",
         packages = setuptools.find_packages(),
         classifiers = (
             "Programming Language :: Python :: 3.5",
